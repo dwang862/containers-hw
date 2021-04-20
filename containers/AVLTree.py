457e16e59ed65ce9305925f94def4eabf5bd6bd7
Daniel Wang <dwang22@cmc.edu>
2021-04-20 00:44:16 -0700

    flake update3

--- containers/AVLTree.py
+++ containers/AVLTree.py
@@ -61,8 +61,9 @@ class AVLTree(BST):
         if not AVLTree._is_bst_satisfied(node):
             return False
         if AVLTree._balance_factor(node) in [-1, 0, 1]:
-            return ret and AVLTree._is_avl_satisfied(node.left) and\
-                   AVLTree._is_avl_satisfied(node.right)
+            right = AVLTree._is_avl_satisfied(node.right)
+            left = AVLTree._is_avl_satisfied(node.left)
+            return ret and left and right
         else:
             return False
 
